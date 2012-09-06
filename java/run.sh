d3d179fcdcb27a62c02e327ffb28dd08a481663a
Ross Manges <ross@callfire.com>
2012-09-06 16:23:19 -0700

    updating callfire-api.jar name

--- java/run.sh
+++ java/run.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-EXAMPLE_CP=lib/commons-lang-2.6.jar:lib/callfire-api-1.0.0-SNAPSHOT.jar:.
+EXAMPLE_CP=lib/commons-lang-2.6.jar:lib/callfire-api.jar:.
 mkdir -p build && javac -classpath $EXAMPLE_CP -d build $1.java && java -classpath $EXAMPLE_CP:build $1
