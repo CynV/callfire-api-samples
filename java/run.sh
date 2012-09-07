749ffa795f5e5fac3d901fc200f29555cf6f6a8e
Ross Manges <ross@callfire.com>
2012-09-06 18:24:02 -0700

    updated dependencies

--- java/run.sh
+++ java/run.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-EXAMPLE_CP=lib/commons-lang-2.6.jar:lib/callfire-api.jar:.
+EXAMPLE_CP=lib/commons-lang-2.6.jar:lib/callfire-api-1.0.0-SNAPSHOT.jar:lib/commons-configuration-1.8.jar:lib/commons-logging-1.1.1.jar:.
 mkdir -p build && javac -classpath $EXAMPLE_CP -d build $1.java && java -classpath $EXAMPLE_CP:build $1
